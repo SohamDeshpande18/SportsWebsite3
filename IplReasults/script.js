94e4b81ec23586f8d25b2c906f205e5f0178162f
SohamDeshpande18 <65907746+SohamDeshpande18@users.noreply.github.com>
2021-03-16 22:25:19 +0530

    Add files via upload

---
+++ IplReasults/script.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA9mKPV1z9M-kmuiEtbUQvPV6waWjMlL_8",
+    authDomain: "sportswebsite2.firebaseapp.com",
+    databaseURL: "https://sportswebsite2-default-rtdb.firebaseio.com",
+    projectId: "sportswebsite2",
+    storageBucket: "sportswebsite2.appspot.com",
+    messagingSenderId: "502929976501",
+    appId: "1:502929976501:web:e2ef6dd4eea0796816e2fc"
+  };
+
+
+  firebase.initializeApp(firebaseConfig);
